fb8c5b65b058b7d0549602b4b8d69975fbc8b900
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-04-26 00:06:56 -0500

    logic for tutorial list component added

--- angular-crud/src/app/components/tutorials-list/tutorials-list.component.ts
+++ angular-crud/src/app/components/tutorials-list/tutorials-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
 
 @Component({
   selector: 'app-tutorials-list',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialsListComponent implements OnInit {
 
-  constructor() { }
+  tutorials: any;
+  currentTutorial: null;
+  currentIndex = -1;
+  title: '';
 
-  ngOnInit() {
+constructor(private tutorialService: TutorialService) { 
+
   }
 
+  ngOnInit() {
+    this.retrieveTutorials();
+  }
+
+  // Retrieve all tutorials
+  retrieveTutorials(){
+    this.tutorialService.getAll().subscribe(data =>{
+      this.tutorials = data;
+      console.log(data);
+    },
+    error => {
+      console.log(error);
+    });
+  }
+
+  // Refresh the tutorials list
+  refreshList(){
+    this.retrieveTutorials();
+    this.currentIndex = null;
+    this.currentIndex = -1;
+
+  }
+  
+  // Set the active tutorial
+  setActiveTutorial(tutorial, index){
+    this.currentIndex = tutorial;
+    this.currentIndex = index;
+  }
+
+  // Remove all tutorials
+  removeAllTutorials(){
+    this.tutorialService.deleteAll().subscribe(response =>{
+      console.log(response);
+      this.retrieveTutorials();
+    },
+    error =>{
+      console.log(error);
+    });
+  }
+
+  // Search a tutorial by title
+  searchTitle(){
+    this.tutorialService.findByTitle(this.title).subscribe(data =>{
+      this.tutorials = data;
+      console.log(data);
+    },
+    error =>{
+      console.log(error);
+    });
+  }
 }
