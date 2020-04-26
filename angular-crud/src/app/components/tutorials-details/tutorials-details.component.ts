1ce63d2dc3ede80043770d1010e1ab2ddd249c86
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-04-26 15:44:49 -0500

    logic for tutorials details component added

--- angular-crud/src/app/components/tutorials-details/tutorials-details.component.ts
+++ angular-crud/src/app/components/tutorials-details/tutorials-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tutorials-details',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialsDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentTutorial = null;
+  message = '';
 
-  ngOnInit() {
+
+  constructor(private tutorialService: TutorialService, private route: ActivatedRoute, private router: Router) {
+
   }
 
+  ngOnInit() {
+    this.message = '';
+    this.getTutorial(this.route.snapshot.paramMap.get('id'));
+  }
+
+  // Get the tutorial by id
+  getTutorial(id){
+    this.tutorialService.get(id).subscribe(data =>{
+      this.currentTutorial = data;
+      console.log(data);
+    },
+    error => {
+      console.log(error);
+    });
+  }
+
+  // Update published tutorials
+  updatePublished(status){
+    const data = {
+      title: this.currentTutorial.title,
+      description: this.currentTutorial.description,
+      published: status
+    };
+
+    this.tutorialService.update(this.currentTutorial.id, data).subscribe(response =>{
+      this.currentTutorial.published = status;
+      console.log(response);
+    },
+    error =>{
+      console.log(error);
+    });
+  }
+
+  // Update a tutorial
+  updateTutorial(){
+    this.tutorialService.update(this.currentTutorial.id, this.currentTutorial).subscribe(response =>{
+      console.log(response);
+      this.message = 'Tutorial updated successfully!';
+    }, 
+    error =>{
+      console.log(error);
+    });
+  }
+
+  // Delete a tutorial 
+  deleteTutorial(){
+    this.tutorialService.delete(this.currentTutorial.id).subscribe(response =>{
+      console.log(response);
+      this.router.navigate(['/tutorials']);
+    },
+    error =>{
+      console.log(error);
+    });
+  }
 }
